d6e2e4bca250d5d403efb8e6dd38232303954f8b
João Marcos <jmcsjoaomarcos@gmail.com>
2024-04-03 21:22:57 +0000

    feat: Adicionado imagem da pag de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image"
+
 const Login = () => {
     return (
-        <>
-        Login
-        <img src="../assets/logo.png" alt="logo" />
-        </>
+        <div className="flex justify-center items-center flex-row">
+            <div className="w-2/4 flex justify-center items-center flex-row">
+                Login
+            </div>
+            <div className="w-2/4 flex m-16 ">
+                <Image src="/image-login.jpg" alt="Login image" width={500} height={500}/>
+            </div>
+        </div>
     )
 }
 
