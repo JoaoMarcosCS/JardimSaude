b9b79498ed4fe588476da7918b18a9c40ded2654
João Marcos <jmcsjoaomarcos@gmail.com>
2024-04-05 11:18:39 +0000

    feat: Tentativa de colocar height 100%

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 const Login = () => {
     return (
-        <div className="flex justify-center items-center flex-row">
-            <div className="w-2/4 flex justify-center items-center flex-row">
+        <div className="flex h-full justify-center items-center flex-row bg-gray-700">
+            <div className="w-2/4 flex justify-center items-center flex-row bg-red-500">
                 Login
             </div>
             <div className="w-2/4 flex m-16 ">
-                <Image src="/image-login.jpg" alt="Login image" width={500} height={500}/>
+                <Image src="/image-login.jpg" alt="Login image" width={500} height={500} layout="responsive"/>
             </div>
         </div>
     )
