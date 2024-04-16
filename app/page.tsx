e6bbe140ea6b56bf74c247b858a698b046049f4b
João Marcos <jmcsjoaomarcos@gmail.com>
2024-04-16 01:36:18 +0000

    feat: Retirada importações inutilizaveis

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 "use client"
 
-import { useSelector } from "react-redux";
-import { IRootState } from "./store/root-reducer";
-import { useRouter } from "next/navigation";
-
-
 export default function Home() {
 
   return (
