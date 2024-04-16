870739272436028cc49204cd0de8170e25cfd0f9
João Marcos <jmcsjoaomarcos@gmail.com>
2024-04-16 11:59:30 +0000

    feat: Adicionada borda na menuBottom

--- app/(home)/components/menuBottom.tsx
+++ app/(home)/components/menuBottom.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 const MenuBottom = () => {
   return (
-    <nav className="w-full hidden justify-center fixed bottom-0 items-center py-4 shadow-md max-[480px]:flex">
+    <nav className="w-full hidden justify-center fixed bottom-0 items-center rounded-md py-4 border-t-2 border-slate-200 max-[480px]:flex">
       <ul className="flex justify-between items-center px-4 w-full">
-        <Link href={""}><li className="flex flex-col justify-center items-center"><Home /> Home</li></Link>
-        <Link href={""}><li className="flex flex-col justify-center items-center"><Stethoscope /> Tratamentos</li></Link>
-        <Link href={""}><li className="flex flex-col justify-center items-center"><SquareUser /> Pacientes</li></Link>
-        <Link href={""}><li className="flex flex-col justify-center items-center"><ClipboardPlus /> Consultas</li></Link>
+        <Link href={""}><li className="flex flex-col justify-center text-xs font-medium items-center"><Home /> Home</li></Link>
+        <Link href={""}><li className="flex flex-col justify-center text-xs font-medium items-center"><Stethoscope /> Tratamentos</li></Link>
+        <Link href={""}><li className="flex flex-col justify-center text-xs font-medium items-center"><SquareUser /> Pacientes</li></Link>
+        <Link href={""}><li className="flex flex-col justify-center text-xs font-medium items-center"><ClipboardPlus /> Consultas</li></Link>
       </ul>
     </nav>
   )
