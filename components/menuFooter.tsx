e9adb4250a1a615f7111130dcea29e6445e9722c
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-04-25 09:24:06 -0300

    feat: Corrigido texto do menuFooter

--- components/menuFooter.tsx
+++ components/menuFooter.tsx
@@ -27,8 +27,8 @@ const MenuFooter = () => {
     <>
     {(!isLoginPage && nivel===2) &&
     <nav className="w-full justify-between fixed bottom-0 items-center rounded-md border-t-2 border-slate-200 max-sm:flex hidden">
-      <ul className="flex justify-between items-center w-full">
-        <ActiveLink href="/" directionTooltip="top" tooltipText="Home"><Home /> aaa</ActiveLink>
+      <ul className="flex justify-between items-center w-full px-2">
+        <ActiveLink href="/" directionTooltip="top" tooltipText="Home"><Home /> Home</ActiveLink>
         <ActiveLink href="/tratamentos" directionTooltip="top" tooltipText="Tratamentos"><Stethoscope /> Tratamentos</ActiveLink>
         <ActiveLink href="/pacientes" directionTooltip="top" tooltipText="Pacientes"><SquareUser /> Pacientes</ActiveLink>
         <ActiveLink href="/consultas" directionTooltip="top" tooltipText="Consultas"><ClipboardPlus /> Consultas</ActiveLink>
