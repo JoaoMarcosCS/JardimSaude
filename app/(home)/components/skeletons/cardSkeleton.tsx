c26c9b1173f9397018794ffc9c3bd01124f5254d
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-04-26 11:27:48 -0300

    feat: add padding

--- app/(home)/components/skeletons/cardSkeleton.tsx
+++ app/(home)/components/skeletons/cardSkeleton.tsx
@@ -2,6 +2,6 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CardSkeleton(){
   return(
-     <Skeleton className="rounded-xl w-[250px] h-[150px]"/>
+     <Skeleton className="rounded-xl w-[330px] h-[150px]"/>
   )
 }
