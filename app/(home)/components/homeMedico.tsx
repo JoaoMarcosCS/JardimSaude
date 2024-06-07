036061080f024454fc2f2383a8a499ddec5bb629
João Marcos <jmcsjoaomarcos@gmail.com>
2024-06-07 14:35:01 +0000

    feat: novas tabelas dos tratamentos para o medico

--- app/(home)/components/homeMedico.tsx
+++ app/(home)/components/homeMedico.tsx
@@ -25,24 +25,26 @@ const HomeMedico = () => {
           <CardTotalTratamentosCancelados />
         </div>
       </div>
-      <div className="container mt-4 shadow-lg">
-        <div className="flex flex-row items-center gap-4">
-          <h1 className="font-bold text-2xl text-yellow-500">Em andamento</h1>
+      <br />
+      <div className="flex justify-center items-center mt-4 w-full ">
+        <div className="flex flex-col items-center border w-11/12 border-slate-100 shadow rounded-md p-4 justify-start">
+          <h1 className="font-bold text-2xl w-full text-start text-yellow-500">Em andamento</h1>
+          <TratamentoTable hookFetchData={useTratamentosEmAndamentoDataByMedicoId()} />
         </div>
-        <TratamentoTable hookFetchData={useTratamentosEmAndamentoDataByMedicoId()}/>
       </div>
-      <div className="container mt-4 shadow-lg">
-        <div className="flex flex-row items-center gap-4">
-          <h1 className="font-bold text-2xl text-green-500">Finalizados</h1>
+      <div className="flex justify-center items-center mt-10 w-full ">
+        <div className="flex flex-col items-center border w-11/12 border-slate-100 shadow rounded-md p-4 justify-start">
+          <h1 className="font-bold text-2xl w-full text-start text-green-500">Finalizados</h1>
+          <TratamentoTable hookFetchData={useTratamentosFinalizadosDataByMedicoId()} />
         </div>
-        <TratamentoTable hookFetchData={useTratamentosFinalizadosDataByMedicoId()}/>
       </div>
-      <div className="container mt-4 shadow-lg">
-        <div className="flex flex-row items-center gap-4">
-          <h1 className="font-bold text-2xl text-red-400">Cancelados</h1>
+      <div className="flex justify-center items-center mt-10 w-full ">
+        <div className="flex flex-col items-center border w-11/12 border-slate-100 shadow rounded-md p-4 justify-start">
+          <h1 className="font-bold text-2xl w-full text-start text-red-500">Cancelados</h1>
+          <TratamentoTable hookFetchData={useTratamentosCanceladosDataByMedicoId()} />
         </div>
-        <TratamentoTable hookFetchData={useTratamentosCanceladosDataByMedicoId()}/>
       </div>
+
     </section>
 
   )
--- app/tratamentos/services/fetchTratamentosFinalizadosByMedicoId.ts
+++ app/tratamentos/services/fetchTratamentosFinalizadosByMedicoId.ts
@@ -1,11 +1,11 @@
-import { FETCH_TOTAL_TRATAMENTOS_FINALIZADOS_BY_MEDICO_ID } from "@/app/constants/apiEndPoints";
+import { FETCH_TOTAL_TRATAMENTOS_FINALIZADOS_BY_MEDICO_ID, FETCH_TRATAMENTOS_FINALIZADOS_BY_MEDICO_ID } from "@/app/constants/apiEndPoints";
 import api from "@/app/services/axios";
 import Cookie from "js-cookie"
 import { Tratamento } from "../interfaces/tratamentoInterface";
 
 const fetchTratamentosFinalizadosByMedicoId = async ():Promise<Tratamento[]> => {
   const id_usuario = Number(Cookie.get("id_usuario"));
-  const response = await api.get(`${FETCH_TOTAL_TRATAMENTOS_FINALIZADOS_BY_MEDICO_ID}/${id_usuario}`);
+  const response = await api.get(`${FETCH_TRATAMENTOS_FINALIZADOS_BY_MEDICO_ID}/${id_usuario}`);
   return response.data;
 }
 
