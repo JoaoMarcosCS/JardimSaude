a9730cc0da00563036438fc681db003717bdf0b3
João Marcos <jmcsjoaomarcos@gmail.com>
2024-06-17 11:23:21 -0300

    Atualizar o menuFooter.tsx

--- app/layout/menuFooter.tsx
+++ app/layout/menuFooter.tsx
@@ -1,4 +1,4 @@
-"use client";
+Ju"use client";
 
 import { ClipboardPlus, Home, Phone, SquareUser, Stethoscope } from "lucide-react";
 import { usePathname } from "next/navigation";
@@ -19,7 +19,7 @@ const MenuFooter = () => {
 
     <>
     {(!isLoginPage && nivel===2) &&
-    <nav className="w-full justify-evenly bg-white fixed bottom-0 items-center rounded-md border-t-2 pb-2 border-slate-200 max-sm:flex hidden">
+    <nav className="w-full justify-evenly bg-white fixed bottom-0 items-center rounded-md border-t-2 pb-2 z-50 border-slate-200 max-sm:flex hidden">
       <ul className="flex justify-between items-center w-full px-2">
         <ActiveLink href="/" directionTooltip="top" tooltipText="Home"><Home /> Home</ActiveLink>
         <ActiveLink href="/tratamentos" directionTooltip="top" tooltipText="Tratamentos"><Stethoscope /> Tratamentos</ActiveLink>
