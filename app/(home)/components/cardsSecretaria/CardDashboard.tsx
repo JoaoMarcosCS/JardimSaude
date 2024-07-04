fa803405f4030d9bd6f5a98ebf8257b39b300cff
João Marcos <jmcsjoaomarcos@gmail.com>
2024-07-04 21:02:42 +0000

    style: card width

--- app/(home)/components/cardsSecretaria/CardDashboard.tsx
+++ app/(home)/components/cardsSecretaria/CardDashboard.tsx
@@ -33,7 +33,7 @@ const CardDashboard = ({ title, tooltipTextHelp, dataToDisplay, icon, color, chi
   }, [])
 
   return (
-    <Card className=" shadow min-w-[330px] max-sm:w-auto" data-aos="fade-up">
+    <Card className=" shadow min-w-[330px] max-sm:flex max-sm:flex-col" data-aos="fade-up">
       <CardHeader className="pb-2">
         <CardDescription className="font-medium text-base gap-1 justify-between px-1 flex items-center">
           <p className="flex flex-row items-center gap-1">{title}
