454c9ac9060a76054c150402c75d5b08335566f4
João Marcos <jmcsjoaomarcos@gmail.com>
2024-07-17 21:29:07 +0000

    fix: tratamento price input

--- app/tratamentos/components/forms/inputs/TratamentoPrice.tsx
+++ app/tratamentos/components/forms/inputs/TratamentoPrice.tsx
@@ -11,12 +11,14 @@ export const TratamentoPrice = ({ name, control, error = null }: CustomInputProp
         control={control}
         render={({ field: { ref, ...rest } }) => (
           <NumericFormat
-          thousandSeparator="."
-          decimalSeparator=","
-          className="border shadow ps-2 rounded py-1.5 border-emerald-100"
-          prefix="R$ "
-          decimalScale={2}
-        />
+            thousandSeparator="."
+            decimalSeparator=","
+            className="border shadow ps-2 rounded py-1.5 border-emerald-100"
+            prefix="R$ "
+            decimalScale={2}
+            getInputRef={ref}
+            {...rest}
+      />
         )}
       />
       {!!error && <Label className="text-red-600">{error?.message}</Label>}
