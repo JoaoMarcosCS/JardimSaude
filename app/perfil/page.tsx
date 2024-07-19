10685e84e5a467a07f65e9d579b88ce5686e638c
João Marcos <jmcsjoaomarcos@gmail.com>
2024-07-19 17:25:58 +0000

    fix

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -13,7 +13,7 @@ const Perfil = () => {
   const { data, isLoading } = useFuncionarioDetailsData(id);
 
   const user = data!
-  
+
   return (
     <section className="mt-2 items-center max-sm:w-full justify-center flex-col flex">
       <h1 className="text-2xl font-semibold text-green-500">Seu perfil</h1>
@@ -31,8 +31,10 @@ const Perfil = () => {
       {
         isLoading ? (
           <Loading />
-        ) :  (
+        ) :  id ? (
           <UserInfo user={user} />
+        ) : (
+          <Loading/>
         )
       }
     </section>
